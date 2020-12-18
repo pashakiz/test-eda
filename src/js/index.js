ab6ad2c2ac38296dc3541221a8f47d664141d7ab
pashakiz <pashakiz@gmail.com>
2020-12-18 23:16:55 +0300

    init prj files

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import * as $ from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
+import '@scss/main.scss'
+import 'owl.carousel'
+
+$(function() {
+
+    $(document).ready(function() {
+
+        //...
+
+    });
+
+});
